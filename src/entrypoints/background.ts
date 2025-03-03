32a6106005b674ddaea62a7aa13370d41ac4eb21
JVM <34409368+tactipus@users.noreply.github.com>
2025-03-03 16:09:27 -0500

    changes to main.ts

--- src/entrypoints/background.ts
+++ src/entrypoints/background.ts
@@ -11,46 +11,42 @@ export default defineBackground(() => {
   /**
   * Gets cookies for x.com and encoding them to base64.
   */
-  console.log("hello 1");
   async function getCookies() {
-    console.log("hello 2");
     // Getting the cookies for the given domain
     let cookies = await browser.cookies.getAll({ domain: domain });
+    console.log(cookies);
         
     // Filter out required cookies
     cookies = cookies.filter(item => item.name == 'auth_token' || item.name == 'ct0' || item.name == 'kdt' || item.name == 'twid');
 
     /** Stores the key */
-    let key = '';
+    let key: string[] = [];
 
-  // If all required cookies are present
+    // If all required cookies are present
     if (cookies.length == 4) {
-      console.log("hello 3");
       // Appending all cookies to a cookie string
       for (const {name, value} of cookies) {
-        key += `${name}=${value};`;
+        key.push(value);
       }
 
       // Encoding the cookies to base64 to get key
-      key = btoa(key);
+      // key = btoa(key);
     }
- 
-    return key;
+      console.log(key);
+	    return key;
+
   }
 
   // Listener for messages from popup
   browser.runtime.onMessage.addListener((request, sender, respond) => {
-    console.log("hello 4");
     if (request.action === 'getCookies') {
       getCookies().then(key => {
         // If key generation was successful
         if (key.length) {
-          console.log("hello 5");
           respond({ success: true, key: key })
         }
         // If key generation failed
         else {
-          console.log("hello 6");
           throw new Error();
         }
       }).catch(err => respond({ success: false }));
